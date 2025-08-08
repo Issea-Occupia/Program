0beaee39eacc3b35876c20fd25f084b0f2b3db46
isseaoccupia <isseaoccupia@gmail.com>
2025-08-08 15:07:31 +0800

    moe

---
+++ moectf2025/getascll.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char character;
+    printf("Type the character\n");
+    scanf("%c",&character);
+    int num = character;
+    printf("Here is tha ascll code:%d",num);
+    return 0;
+}
