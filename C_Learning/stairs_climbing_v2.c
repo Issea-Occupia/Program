58d78df9f3d81bec6271e70d36b8c2e444076bcc
isseaoccupia <isseaoccupia@gmail.com>
2025-08-10 16:27:40 +0800

    climbing the steps

--- C_Learning/stairs_climbing_v2.c
+++ C_Learning/stairs_climbing_v2.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
-int f(int);
+long long f(int);
 int main(){
     int steps;
     do
     {
-        printf("Here is a man climbs the stair,type the number of steps: ");
+        printf("Here is a man climbs the stair,type the number of steps(positive interge): ");
         scanf("%d",&steps);
     } while (steps < 0);
-    printf("He has %d methods to climb.",f(steps));
+    printf("He has %lld methods to climb.",f(steps));
 }
-int f(int steps){
-    int a =1;
-    int b =1;
-    int c =1;
-    int frequency =1;
-    while (frequency < steps)
+long long f(int steps){
+    long long a =1;
+    long long b =1;
+    long long c =1;
+    if (steps <= 2)
     {
-        c = a + b;
-        int temp_a = b;
-        a = temp_a;
-        b = c;
-        frequency++;
+        return steps;
+    }
+    else{
+        for (int i = 1; i < steps; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
     }
     return c; 
 }
+/*f代表函数，表示了楼梯数目与攀爬方式的映射关系*/
