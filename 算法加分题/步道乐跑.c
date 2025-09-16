485bafea7c7bbb08a952a603e0f2d446d2794816
isseaoccupia <isseaoccupia@gmail.com>
2025-09-16 16:12:41 +0800

    更改

--- 算法加分题/步道乐跑.c
+++ 算法加分题/步道乐跑.c
@@ -6,18 +6,28 @@ int main(){
     scanf("%d",&number);
     char ** runners = (char **)malloc(sizeof(char *) * number);
     int * times = (int *)malloc(sizeof(int) * number);
+    int ** dataset = (int **)malloc(sizeof(int *) * number);
     for (size_t i = 0; i < number; i++)
     {
-        *(runners + i) = (char *)malloc(sizeof(char) * 101);
+        *(runners + i) = (char *)malloc(sizeof(char) * 21);
         scanf("%s",*(runners + i));
         scanf("%d",times + i);
-        int * detailed_data = (int)malloc(sizeof(int) * *(times + i));
+        *(dataset + i) = (int *)malloc(sizeof(int) * *(times + i));
         /*接受跑步人跑步数据*/
-        for (size_t j = 0; i < *(times + i); j++)
+        for (size_t j = 0; j < *(times + i); j++)
         {
-            scanf("%d",detailed_data + j);
+            scanf("%d",(*(dataset + i) +j));
         }
     }
     /*判断总里程*/
+    int * odometer = (int*)malloc(sizeof(int) * number);
+    for (size_t i = 0; i < number; i++)
+    {
+        for (size_t j = 0; j < *(times +i); i++)
+        {
+            
+        }
+        
+    }
     
 }
