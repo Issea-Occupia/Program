0cd75abfb3805e896fccbcc81bcaa71ead38eff9
isseaoccupia <isseaoccupia@gmail.com>
2025-10-02 16:57:51 +0800

    change

--- python/PyQt6_learning/pyqt_learning_ch1.py
+++ python/PyQt6_learning/pyqt_learning_ch1.py
@@ -7,16 +7,21 @@ class MainWindows(QMainWindow):
 
         self.setWindowTitle("my first window")
         button = QPushButton("push me!!")
+        button.setCheckable(True)
+        button.clicked.connect(self.isclicked)
+        button.clicked.connect(self.istoggled)
         minisize = QSize(300,400)
         maxsize = QSize(600,800)
 
         self.setCentralWidget(button)
         self.setMinimumSize(minisize)
         self.setMaximumSize(maxsize)
-
+    def isclicked(self):
+        print("the button is clicked!")
+    def istoggled(self,checked):
+        print("the button is toggled!",checked)
 app = QApplication(sys.argv)
 
 window = MainWindows()
 window.show()
-
 app.exec()
