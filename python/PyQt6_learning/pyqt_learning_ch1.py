d80ee33572585eb0a7919bc15fa4089bb4c560cf
isseaoccupia <isseaoccupia@gmail.com>
2025-10-03 00:19:20 +0800

    change

--- python/PyQt6_learning/pyqt_learning_ch1.py
+++ python/PyQt6_learning/pyqt_learning_ch1.py
@@ -1,14 +1,13 @@
-import sys
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 class MainWindows(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.ischecked = True
         self.setWindowTitle("my first window")
         button = QPushButton("push me!!")
         button.setCheckable(True)
-        button.setChecked(True)
+        button.setChecked(self.ischecked)
         button.clicked.connect(self.isclicked)
         button.clicked.connect(self.istoggled)
         minisize = QSize(300,400)
@@ -20,8 +19,9 @@ class MainWindows(QMainWindow):
     def isclicked(self):
         print("the button is clicked!")
     def istoggled(self,checked):
+        self.ischecked = checked
         print("the button is toggled!",checked)
-app = QApplication(sys.argv)
+app = QApplication([])
 
 window = MainWindows()
 window.show()
