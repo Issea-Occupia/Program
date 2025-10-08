62412938855cdb9828a196163962b782b6fea6c4
isseaoccupia <isseaoccupia@gmail.com>
2025-10-08 11:22:22 +0800

    reorganise my structure

--- python/PyQt6_learning/attempt on qlineedit.py
+++
@@ -1,2 +0,0 @@
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import * 
---
+++ python/PyQt6_learning/component/attempt on qlineedit.py
@@ -0,0 +1,37 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import * 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.lineedit = QLineEdit()
+        self.lineedit.setMaxLength(20)
+        maxlen = self.lineedit.maxLength()
+
+        self.lineedit.setPlaceholderText("type your name:")
+        placetext = self.lineedit.placeholderText()
+        self.lineedit.returnPressed.connect(self.return_pressed)
+        self.lineedit.selectionChanged.connect(self.selection_changed)
+        self.lineedit.textChanged.connect(self.text_changed)
+        self.lineedit.textEdited.connect(self.text_edited)
+
+        self.setCentralWidget(self.lineedit)
+
+    def return_pressed(self):
+        print("Return pressed!")
+        self.centralWidget().setText("boom")
+
+    def selection_changed(self):
+        print("Selection changed")
+    
+    def text_changed(self):
+        print("Text changed...")
+
+    def text_edited(self):
+        print("Text edited...")
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
