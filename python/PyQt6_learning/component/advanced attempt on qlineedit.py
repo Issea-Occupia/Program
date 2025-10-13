3c9ed4594b5e07af724fb711fc6cba362f1c8fe3
isseaoccupia <isseaoccupia@gmail.com>
2025-10-13 16:19:01 +0800

    i need to use pyside6,so i delete unfit pyqt source code

--- python/PyQt6_learning/component/advanced attempt on qlineedit.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QLineEdit,QApplication,QMainWindow
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.widget = QLineEdit()
-        self.widget.setText("please type your name")
-
