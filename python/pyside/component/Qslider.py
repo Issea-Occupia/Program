903cfecfcbe163a50de9c5cefb4f795ec77f1e31
isseaoccupia <isseaoccupia@gmail.com>
2025-10-15 18:12:14 +0800

    将小玩意结构化了一下

--- python/pyside/component/Qslider.py
+++ python/pyside/component/Qslider.py
@@ -1,33 +1,47 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QSlider, QWidget, QVBoxLayout, QHBoxLayout, QLabel
+from PySide6.QtWidgets import QApplication, QMainWindow, QSlider, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QCheckBox
 from PySide6.QtCore import Qt
+from random import randint
+a = randint(-100, 100)
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle("My Window")
+        self.setwidgets()
+        self.setwidget()
 
-        widget = QSlider(Qt.Horizontal)
-        widget.setRange(-100, 100)
-        widget.setValue(0)
-        widget.setSingleStep(1)
+    def setwidgets(self):
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(-100)
+        self.slider.setMaximum(100)
+        self.slider.setSingleStep(1)
 
-        widget.valueChanged.connect(self.value_changed)
+        self.label = QLabel(self)
+        text = self.slider.value()
+        self.label.setText(str(text))
 
+        self.checkbox = QCheckBox(self)
+        self.checkbox.setChecked(True)
+        self.checkbox.stateChanged.connect(self.changevisibility)
+        self.slider.valueChanged.connect(self.setlabletext)
+
+    def setwidget(self):
+        layout = QHBoxLayout()
         container = QWidget()
-        layout =  QHBoxLayout()
-        layout.addWidget(widget)
-        self.lable = QLabel()
-        layout.addWidget(self.lable)
+        layout.addWidget(self.label)
+        layout.addWidget(self.checkbox)
+        layout.addWidget(self.slider)
         container.setLayout(layout)
         self.setCentralWidget(container)
 
+    def changevisibility(self,i):
+        self.slider.setVisible(i)
 
-    def value_changed(self,i):
-        self.lable.setText(str(i))
-
+    def setlabletext(self,text):
+        self.label.setText(str(text))
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 
-app.exec_()
+app.exec()
 
